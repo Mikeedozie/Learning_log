10a092129063097c511a1c86696cf534168eae35
Mikeedozie <odigwee12@gmail.com>
2022-05-24 21:55:05 +0000

    code

--- learning_log/.ipynb_checkpoints/settings-checkpoint.py
+++ learning_log/.ipynb_checkpoints/settings-checkpoint.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-(4e6+_!t1q#@(^6*k63ej3c$ll%jc27-uyepc7!av688u=$e%%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['geek-buddy.herokuapp.com']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
