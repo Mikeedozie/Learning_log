4f4d87f8e930280d75f89a910f367e3920863652
Mikeedozie <odigwee12@gmail.com>
2022-05-25 07:27:58 +0000

    change of db

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,11 +86,10 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+            'default': dj_database_url.config(default='postgres://localhost')
+
     }
-}
+
 
 
 
